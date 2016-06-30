9518f64e4ef13aed203d97f70f48bc6284015964
Rosechelle Joy Oraa <rosechellejoyoraa@gmail.com>
2016-06-30 09:27:39 +0800

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-arg = "$1"
-perl createAlignmentSlurm.pl input.info $arg
-step1 = sbatch ../analysis/$arg/submit_slurm.sh
 
-perl createBAMProcessingSlurm.pl input.info $arg
-step2 = sbatch --dependency=afterok:$step1 ../analysis/$arg/submit_sam2bam_slurm.sh
+chmod u+x format_reference.sh
+./format_reference
 
-step3 = sbatch --dependency=afterok:$step2 python mergebam.py -g ../analysis/<arg1>
+perl createAlignmentSlurm.pl input.info $1
+step1 = $(sbatch home/rosechelle.oraa/analysis/$1/submit_slurm.sh)
 
-bam2vcf.py -b ../scratch2/output -r ../reference -g ../software/GenomeAnalysisTK-3.2-2 -t temp_dir
+perl createBAMProcessingSlurm.pl input.info $1
+step2 = $(sbatch --dependency=afterany:$step1 ../analysis/$1/submit_sam2bam_slurm.sh)
 
-sbatch --dependency=afterany:$step3 vcfToTable.sh
-
-
-
-
-
+step3=(sbatch --dependency=afterok:$step2 mergebamslurm.sh)
+#perl mergebam.pl home/rosechelle.oraa/scratch2/output/ IRIS-313-10519
+#python bam2vcf.py -b ../scratch2/output -r ../reference -g ../software/GenomeAnalysisTK-3.2-2 -t temp_dir
+#gzip ../scratch2/output/*.vcf 
