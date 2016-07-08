50473cce029d59ae0986e4637b163e5ceb59d537
Rosechelle Joy Oraa <rosechellejoyoraa@gmail.com>
2016-07-08 11:39:09 +0800

    added sleep after each sbatch

--- run3.sh
+++ run3.sh
@@ -9,73 +9,62 @@ filename="input.info"
 inc_pairs=false
 
 #check if read pairs are complete
-while read -r line
+while read -r line		#read each line
 do
         IFS=':' read -ra info <<< "$line" #split, get each genome
-        for pair in `ls $input_dir/$info/*1.fq.gz`
+        for pair in `ls $input_dir/$info`
 		do
-			pair2=${pair/1.fq.gz/2.fq.gz}
+			if [[ $pair == *"1.fq.gz"* ]]
+			then
+  				pair2="$input_dir/$info/${pair/1.fq.gz/2.fq.gz}"
+			elif [[ $pair == *"2.fq.gz"* ]]
+			then
+				pair2="$input_dir/$info/${pair/2.fq.gz/1.fq.gz}"	
+			fi 
+
 			if [ ! -f $pair2 ]
 			then
 				echo "$pair does not have a pair"
-				inc_pairs=true
+				inc_pairs=true		
 			fi
 		done
 done < "$filename"
- 
-while read -r line
-do
-        IFS=':' read -ra info <<< "$line" #split, get each genome
-        for pair in `ls $input_dir/$info/*2.fq.gz`
-                do
-                       pair2=${pair/2.fq.gz/1.fq.gz}
-                       if [ ! -f $pair2 ]
-                       then
-				
-                                echo "$pair does not have a pair"
-                       		inc_pairs=true
-                       fi
-                done
-done < "$filename"
 
-if [ "$inc_pairs" = true ]
+
+if [ "$inc_pairs" = true ]	#if there are incomplete pairs
 then
-	exit $
+	exit $			#exit program
 fi
 
-format=$(sbatch format_reference.sh)
+format=$(sbatch format_reference.sh)		#format reference
 
-perl createAlignmentSlurm.pl $filename $disk
+perl createAlignmentSlurm.pl $filename $disk		#create slurm scripts for each step
 perl createBAMProcessingSlurm.pl $filename $disk
 perl createMergeBAMSlurm.pl $filename $disk
 perl createBAMtovcfslurm.pl $filename $disk
 
-while read -r line
+while read -r line			#read each line to get the genomes
 do
 	IFS=':' read -ra info <<< "$line" #split, get each genome
+	job=`ls $analysis_dir/$disk/$info/*fq2sam.* `
 
-	for job in `ls $analysis_dir/$disk/$info/*fq2sam.* `
-                do      #per read pair
-			#submit *fq2sam. to the job scheduler, set format reference as its dependency
-                        dep=$(sbatch --dependency=afterok:${format##* } $job)
-                        samtobam=${job/fq2sam./sam2bam.}
-			#submit its corresponding sam2bam to the job scheduler w/ fq2sam as its dependency
-			sbatch --dependency=afterok:${dep##* } $samtobam
-                done
+        #submit *fq2sam. to the job scheduler, set format_reference as its dependency
+        dep=$(sbatch --dependency=afterok:${format##* } $job)
+	samtobam=${job/fq2sam./sam2bam.}
+	#submit its corresponding sam2bam to the job scheduler w/ fq2sam as its dependency
+	sbatch --dependency=afterok:${dep##* } $samtobam
+        sleep 5        
 done < "$filename"
 
 while read -r line
 do
 	IFS=':' read -ra info <<< "$line" #split, get each genome
-
-        for job in `ls $analysis_dir/$disk/$info/*mergebam.* `
-                do      #per read pair
-                        #submit *mergebam. to the job scheduler, set all sam2bam as its dependency
-                        dep=$(sbatch --dependency=singleton $job)
-                        bamtovcf=${job/mergebam./bam2vcf.}
-                        #submit its corresponding bam2vcf to the job scheduler w/ mergebam as its dependency
-                        sbatch --dependency=afterok:${dep##* } $bamtovcf
-                done
+	job=`ls $analysis_dir/$disk/$info/*mergebam.* `
+        
+        #submit *mergebam. to the job scheduler, set all sam2bam of the same genome as its dependency
+        dep=$(sbatch --dependency=singleton $job)
+        bamtovcf=${job/mergebam./bam2vcf.}
+        #submit its corresponding bam2vcf to the job scheduler w/ mergebam as its dependency
+        sbatch --dependency=afterok:${dep##* } $bamtovcf
+        sleep 5
 done < "$filename"
-	
-
