183b71dfcdee61a39bf4781169729c46f281ffdb
Rosechelle Joy Oraa <rosechellejoyoraa@gmail.com>
2016-07-22 16:45:40 +0800

    added program description

--- mergebam.pl
+++ mergebam.pl
@@ -1,3 +1,9 @@
+#       Filename: mergebam.pl
+#       Description: submits the command for merging bam files
+#       Parameters: output directory, genome dir/raw directory
+#       Created by: Jeffrey Detras
+#	Modified by:  Rosechelle Joy Oraa
+
 #!/usr/bin/perl -w
 use strict;
 
