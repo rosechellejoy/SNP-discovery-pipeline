cacce133859aa9d3ca81e78dce81ca33dd697bbf
Rosechelle Joy Oraa <rosechellejoyoraa@gmail.com>
2016-07-27 08:51:46 +0800

    modified output filename

--- createInput.sh
+++ createInput.sh
@@ -4,11 +4,11 @@ input_dir=${conf##*=}
 conf=`grep -n "disk=" config`
 disk=${conf##*=}
 #remove previous input.info
-rm try 
+rm input.info 
 
 #create new input.info
 #list all genomes with no of reads
 for genome in `ls -d $input_dir/*`; do
 	count=`ls $genome | wc -l`
-	echo ${genome##*/}:$count >> try
+	echo ${genome##*/}:$count >> input.info
 done
